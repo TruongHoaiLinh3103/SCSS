74fe1fb5867fa16fa2a4f9657c4c456794a8bfdf
Anikey <eagleremlinh@gmail.com>
2024-06-26 01:13:55 +0700

    Scss

---
+++ js/js-ggcn.js
@@ -0,0 +1,38 @@
+function ggsy(){
+
+
+
+
+ document.writeln("<script async src=\'//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\'></script>");
+document.writeln("<!-- sass -->");
+document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("     style=\'display:block\'");
+document.writeln("     data-ad-client=\'ca-pub-8390375436380642\'");
+document.writeln("     data-ad-slot=\'8139316811\'");
+document.writeln("     data-ad-format=\'auto\'></ins>");
+document.writeln("<script>");
+document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("</script>");
+
+}
+
+
+function ggny1(){
+
+ document.writeln("<script async src=\'//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\'></script>");
+document.writeln("<!-- sass -->");
+document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("     style=\'display:block\'");
+document.writeln("     data-ad-client=\'ca-pub-8390375436380642\'");
+document.writeln("     data-ad-slot=\'8139316811\'");
+document.writeln("     data-ad-format=\'auto\'></ins>");
+document.writeln("<script>");
+document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("</script>");
+
+}
+
+function ggny2(){
+ 
+}
+
---
+++ js/js-tongji.js
@@ -0,0 +1,9 @@
+
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?792b93109e4fad5c983efc03172eaaec";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
